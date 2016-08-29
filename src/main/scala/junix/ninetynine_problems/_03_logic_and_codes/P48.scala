f95ab548f7a8b96a80fc2944534b554da079f094
wanglijun <juns1984@qq.com>
2016-08-29 09:44:13 +0800

    fix

---
+++ src/main/scala/junix/ninetynine_problems/_03_logic_and_codes/P48.scala
@@ -0,0 +1,10 @@
+package junix.ninetynine_problems._03_logic_and_codes
+
+/**
+  * 3.03 (**) Truth tables for logical expressions (3).
+  *
+  * Skipping this problem for now.
+  */
+object P48 {
+
+}
